a442009b76c7225df70075e127d94e707ed773bf
klquast <klquast8807@gmail.com>
2013-10-02 19:59:42 -0500

    Added Index and About pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,10 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Your new application is ready", "Kaytee"))
   }
 
+  def about = Action {
+    Ok(views.html.about())
+  }
 }
