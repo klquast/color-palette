a78c4f95534046d54d29450d76dd767ff0870ec5
klquast <klquast8807@gmail.com>
2013-10-08 21:57:08 -0500

    Drag and Drop, saturation and brightness calculation

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,4 +3,27 @@ $(function(){
         $('body').toggleClass('dark');
         //$(this).toggleClass('btn-primary').toggleClass('btn-default')
     });
+
+    $( ".selected-color-circle" ).draggable({
+        containment: ".color-picker",
+        scroll: false,
+        drag: function() {
+            var saturation = calcSaturation();
+            var brightness = calcBrightness();
+            $('.saturation').html(saturation);
+            $('.brightness').html(brightness);
+        }
+    });
+
+    function calcSaturation() {
+        var pos = $('.selected-color-circle').css('left')
+        pos = parseInt(pos);
+        return Math.round((pos + 8)/2.56);
+    }
+
+    function calcBrightness() {
+        var pos = $('.selected-color-circle').css('top');
+        pos = parseInt(pos);
+        return Math.round(100 - (pos + 8) / 2.56);
+    }
 });
