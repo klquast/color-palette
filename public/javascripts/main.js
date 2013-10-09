65a627f7f70c1c0787ee0757ab9cbd46873ac519
klquast <klquast8807@gmail.com>
2013-10-09 18:11:36 -0500

    Hue slider actually changes the bg color for the picker

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,34 @@
 $(function(){
     $('.toggle-bg-color').click(function(){
         $('body').toggleClass('dark');
-        //$(this).toggleClass('btn-primary').toggleClass('btn-default')
     });
 
+    /* Make selected-color-circle draggable */
     $('.selected-color-circle').draggable({
         containment: ".color-picker",
         scroll: false,
         drag: function(e) {
-            var saturation = calcSaturation();
-            var saturation = calcSaturation();
-            var brightness = calcBrightness();
+            var saturation = colors.saturation();
+            var brightness = colors.brightness();
             $('.saturation').html(saturation);
             $('.brightness').html(brightness);
         }
     });
 
+    /* Make selected-color-circle draggable */
+    $('.selected-hue-arrows').draggable({
+        axis: 'y',
+        containment: ".hue-slider",
+        scroll: false,
+        drag: function() {
+            var hue = colors.hue();
+            var rgb = convert.hsvToRgb(hue, 100, 100);
+            $('.color-picker-backdrop').css('background', 'rgb(' + rgb + ')');
+            $('.hue').html(hue);
+        }
+    });
+
+    /* Click (mousedown) on the color picker image */
     $('body').on('mousedown', '.color-picker-backdrop', function(e){
         var posX = $(this).offset().left,
             posY = $(this).offset().top,
@@ -27,15 +40,15 @@ $(function(){
             top: (e.pageY - posY) - 8.5,
             left: (e.pageX - posX) - 8
         });
-        var saturation = calcSaturation();
-        var brightness = calcBrightness();
+        var saturation = colors.saturation();
+        var brightness = colors.brightness();
         $('.saturation').html(saturation);
         $('.brightness').html(brightness);
-        $('.pixel-offset').html(((e.pageX - posX) - 8)+ ' , ' + ((e.pageY - posY) - 8.5));
 
         $circle.triggerHandler(e);
     });
 
+    /* Click (mousedown) on the hue slider image */
     $('body').on('mousedown', '.hue-slider-backdrop', function(e){
         var posY = $(this).offset().top,
             $arrows = $('.selected-hue-arrows');
@@ -45,39 +58,85 @@ $(function(){
         $arrows.css({
             top: (e.pageY - posY) - 8.5
         });
-        var hue = calcHue();
-        $('.hue').html(hue);
-        $('.pixel-offset').html((e.pageY - posY) - 8.5);
+        var hue = colors.hue();
 
+        $('.hue').html(hue);
+        var rgb = convert.hsvToRgb(hue, 100, 100);
+        $('.color-picker-backdrop').css('background', 'rgb(' + rgb + ')');
         $arrows.triggerHandler(e);
     });
+});
 
-    $('.selected-hue-arrows').draggable({
-        axis: 'y',
-        containment: ".hue-slider",
-        scroll: false,
-        drag: function() {
-            var hue = calcHue();
-            $('.hue').html(hue);
-        }
-    });
-
-    function calcHue() {
+/***************
+*   FUNCTIONS
+***************/
+var colors = {
+    hue: function(){
         var pos = $('.selected-hue-arrows').css('top')
         pos = parseInt(pos);
         var div = 256/360;
         return 360 - Math.round((pos + 8)/div);
-    }
-
-    function calcSaturation() {
+    },
+    saturation: function(){
         var pos = $('.selected-color-circle').css('left')
         pos = parseInt(pos);
         return Math.round((pos + 8)/2.56);
-    }
-
-    function calcBrightness() {
+    },
+    brightness: function(){
         var pos = $('.selected-color-circle').css('top');
         pos = parseInt(pos);
         return Math.round(100 - (pos + 8) / 2.56);
     }
-});
+};
+
+var convert = {
+    hsvToRgb: function(hue, sat, value){
+        var r, g, b;
+        hue /= 360;
+        sat /= 100;
+        value /= 100;
+
+        var i = Math.floor(hue * 6);
+        var f = hue * 6 - i;
+        var p = value * (1 - sat);
+        var q = value * (1 - f * sat);
+        var t = value * (1 - (1 - f) * sat);
+
+        switch (i % 6) {
+            case 0: r = value, g = t, b = p; break;
+            case 1: r = q, g = value, b = p; break;
+            case 2: r = p, g = value, b = t; break;
+            case 3: r = p, g = q, b = value; break;
+            case 4: r = t, g = p, b = value; break;
+            case 5: r = value, g = p, b = q; break;
+        }
+
+        r = Math.round(r * 255);
+        g = Math.round(g * 255);
+        b = Math.round(b * 255);
+
+        return [r, g, b];
+    },
+    rgbToHsv: function(red, green, blue){
+        var h, s, v;
+
+        red /= 255;
+        green /= 255;
+        blue /= 255;
+
+        var maxRgb = Math.max(red, green, blue);
+        var minRgb = Math.min(red, green, blue);
+
+        if(minRgb == maxRgb) {
+            return [0, 0, minRbg];
+        }
+
+        var d = (red == minRgb) ? green - blue : ((blue == minRgb) ? red - green : blue - red);
+        var hd = (red == minRgb) ? 3 : ((blue == minRgb) ? 1 : 5);
+        h = 60 * (hd - d / (maxRgb - minRgb));
+        s = (maxRgb - minRgb) / maxRgb;
+        v = maxRgb;
+
+        return [Math.round(h), Math.round(s*100), Math.round(v*100)];
+    }
+};
